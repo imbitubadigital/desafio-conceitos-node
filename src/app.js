40d7ee5dac725c3989bff7ddf083c3b6b46a85e9
Tony Fernandes <imbitubadigital@gmail.com>
2020-04-20 14:46:55 -0300

    Desafio concluído

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,64 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(400).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.status(400).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  const { id } = request.params;
+  const repository = repositories.find(repo => repo.id === id);
+  if(!repository){
+    return response.status(400).json({ message: 'Repositório não localizado!'});
+  }
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+  return response.status(400).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+
+  const repository = repositories.find(repo => repo.id === id);
+  if(!repository){
+    return response.status(400).json({ message: 'Repositório não existe!'});
+  }
+
+  repositories.map((r, index) => {
+    if(r.id === id){
+      repositories.splice(index, 1);
+    }
+  });
+
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.find(repo => repo.id === id);
+  if(!repository){
+    return response.status(400).json({ message: 'Repositório nao encontrado!'});
+  }
+  repository.likes ++;
+  return response.status(400).json(repository);
 });
 
 module.exports = app;
